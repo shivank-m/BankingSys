9ff77c9b4cac32366b656fd3c2d8646e1fdedbef
shivank-m <105384074+shivank-m@users.noreply.github.com>
2023-03-20 00:25:38 +0530

    Add files via upload

---
+++ idonate.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>iDonate</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    include 'navigation.php';
+    ?>
+
+  <div class="container-fluid">
+    <div class="row intro py-5">
+      <div class="col-sm-12 col-md">
+        <div class="heading text-center my-5">
+          <h1>iDonate</h1>
+          <h3>Your donation can make a difference in someone's life and bring a positive impact on the world.</h3>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md text-center my-5">
+        <br>
+      <a href="https://rzp.io/l/C3IWMaoI1"><button>Donate Now</button></a>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
